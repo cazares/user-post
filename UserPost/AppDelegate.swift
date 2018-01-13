e8e602ecdf97c2362441ffe974b57283546d6208
Miguel Cazares <miguel.cazares@shipstation.com>
2018-01-13 12:59:06 -0600

    move constants to constants file

--- UserPost/AppDelegate.swift
+++ UserPost/AppDelegate.swift
@@ -8,12 +8,6 @@
 
 import UIKit
 
-let topNavColor = UIColor(red: 206.0/255.0, green: 17.0/255.0, blue: 41.0/255.0, alpha: CGFloat(1.0))
-let bgColor = UIColor(red: 238.0/255.0, green: 238/255.0, blue: 238/255.0, alpha: CGFloat(1.0))
-let emptyString = ""
-let padding = CGFloat(10.0)
-let cancel = "Cancel"
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
---
+++ UserPost/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  UserPost
+//
+//  Created by Miguel Cazares on 1/13/18.
+//  Copyright © 2018 Miguel Cazares. All rights reserved.
+//
+
+import UIKit
+
+let topNavColor = UIColor(red: 206.0/255.0, green: 17.0/255.0, blue: 41.0/255.0, alpha: CGFloat(1.0))
+let bgColor = UIColor(red: 238.0/255.0, green: 238/255.0, blue: 238/255.0, alpha: CGFloat(1.0))
+let emptyString = ""
+let padding = CGFloat(10.0)
+let cancel = "Cancel"
+
+class Constants: NSObject { }
