4be8c603e75a77aca89b9e7930694332c505119a
Miguel Cazares <miguel.cazares@shipstation.com>
2018-01-13 16:29:11 -0600

    convert Post object to decodable method instead of Mantle

--- UserPost/Model/Post.swift
+++ UserPost/Model/Post.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
-import Mantle
 
-class Post: MTLModel, MTLJSONSerializing {
-    var body = emptyString
-    var userId = 0
-    var title = emptyString
-    var id = 0
+struct Post: Decodable {
+    let body: String
+    let userId: Int
+    let title: String
+    let id: Int
     
-    static func jsonKeyPathsByPropertyKey() -> [AnyHashable : Any]! {
-        return [ "body": "body",
-                 "userId": "userId",
-                 "title": "title",
-                 "id": "id" ]
+    enum PostKeys: String, CodingKey {
+        case body, userId, title, id
+    }
+    
+    init(body: String, userId: Int, title: String, id: Int) {
+        self.body = body
+        self.userId = userId
+        self.title = title
+        self.id = id
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: PostKeys.self)
+        
+        let body = try container.decode(String.self, forKey: .body)
+        let userId = try container.decode(Int.self, forKey: .userId)
+        let title = try container.decode(String.self, forKey: .title)
+        let id = try container.decode(Int.self, forKey: .id)
+        
+        self.init(body: body, userId: userId, title: title, id: id)
     }
 }
